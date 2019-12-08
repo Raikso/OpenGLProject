948ad7b75e7ed86c48ee09554c50abe185f60a4a
Eric Stark <Raikso@users.noreply.github.com>
2019-12-08 22:14:08 +0100

    Added code for missing MeshTypes. Added code to main to init and draw FlatModels.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-void renderScene()
-{
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glClearColor(1.0, 0.0, 0.0, 1.0); // clear red
+#include "ShaderLoader.h"
+#include "Camera.h"
+#include "LightRenderer.h"
 
-	// draw game objects here
+Camera* camera;
+LightRenderer* light;
 
-}
+void initGame();
+void renderScene();
 
+// main
 int main(int argc, char** argv)
 {
 	glfwInit();
@@ -17,6 +19,10 @@ int main(int argc, char** argv)
 	GLFWwindow* window = glfwCreateWindow(800, 600, " Hello OpenGL ", NULL, NULL);
 
 	glfwMakeContextCurrent(window);
+
+	glewInit();
+
+	initGame();
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -31,5 +37,32 @@ int main(int argc, char** argv)
 
 	glfwTerminate();
 
+	delete camera;
+	delete light;
+
 	return 0;
+}
+
+// methods
+void renderScene()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(1.0, 1.0, 0.0, 1.0); // clear yellow
+	light->draw();
+
+	// draw game objects here
+
+}
+void initGame()
+{
+	glEnable(GL_DEPTH_TEST);
+
+	ShaderLoader shader;
+	GLuint flatShaderProgram = shader.CreateProgram("Assets/Shaders/FlatModel.vs", "Assets/Shaders/FlatModel.fs");
+
+	camera = new Camera(45.0f, 800, 600, 0.1f, 100.0f, glm::vec3(0.0f, 4.0f, 6.0f));
+
+	light = new LightRenderer(MeshType::kTriangle, camera);
+	light->setProgram(flatShaderProgram);
+	light->setPosition(glm::vec3(0.0f, 0.0f, 0.0f));
 }
